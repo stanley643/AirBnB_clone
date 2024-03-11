9ec3f0d2c93073ad8de7c170bef1fc6b903d2ca4
stanley643 <nmurigi@kabarak.ac.ke>
2024-03-11 12:28:45 -0700

    code a code

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 from datetime import datetime
 from models.base_model import BaseModel
-from models import storage
+from models.engine import file_storage
 from models.user import User
 from models.state import State
 from models.city import City
--- models/__init__.py
+++ models/__init__.py
@@ -8,3 +8,4 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import models.engine.file_storage
